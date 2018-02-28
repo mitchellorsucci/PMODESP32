1d158f74b3728e1a84cbc5cfad665cab3d332274
Mitchell <mitchellorsucci@gmail.com>
2018-02-28 13:51:53 -0800

    Added functioning server example using ssl in python. Added some HTML, JS, and CSS that runs well thus far

--- ESP32.h
+++ ESP32.h
@@ -1,3 +1,20 @@
+/*******************************************************************************
+    The AT commands utilized here for interfacing with the ESP32 are found
+    in the document ESP32_AT_Instruction_Set_and_Example__EN provided by
+    ESPRESSIF> This library does not implement all of the AT commands that
+    are available to be used. If the user wishes to use an AP command that is
+    not included in the library, the user can implement it in much the same
+    way that these AT commands are implemented
+*******************************************************************************/
+
+/*******************************************************************************
+    Revision History
+
+    01/26/18        Initial Release         Mitchell Orsucci
+                                            (mitchell.orsucci@ni.com)
+
+*******************************************************************************/
+
 #ifndef ESP32_H
 #define ESP32_H
 
@@ -11,25 +28,66 @@
 #include "xil_exception.h"
 #include "xuartlite_l.h"
 #include <stdio.h>
+#include <unistd.h>
 
+//#define UART_BUF_SIZE			16
+//#define UART_BUF_SIZE			15
+#define UART_BUF_SIZE			14
 
+/*************************** XILINX ARGUMENT MACROS ***************************/
 #define UARTLITE_DEVICE_ID      XPAR_UARTLITE_1_DEVICE_ID
 #define INTC_DEVICE_ID          XPAR_INTC_0_DEVICE_ID
 #define UARTLITE_INT_IRQ_ID     XPAR_INTC_0_UARTLITE_1_VEC_ID
 
-#define AT_STUB					"AT"
-
-// Typedefs for cleaner code
+/***************************** TYPEDEFs ***************************/
 typedef XUartLite         	    Uart;
 #define INTC                    XIntc
 #define INTC_HANDLER            XIntc_InterruptHandler
 
+/****************************** WIFI COMMAND MACROS ***************************/
+    // Wifi mode macros
+#define NULL_MODE				0
+#define STATION_MODE			1
+#define SOFTAP_MODE				2
+#define SOFTAP_AND_STATION_MODE	3
 
+    // Encryption mode macros for SoftAP mode
+#define NO_PASSWORD             0
+#define WPA_PSK                 2
+#define WPA2_PSK                3
+#define WPA_WPA2_PSK            4
+
+/****************************** AT Control Utilities **************************/
 int initATCtrl(u32 UART_DEVICE_ID, Uart * devicePtr, INTC * intPtr);
 int resetESP32(Uart * devicePtr);
+int checkVersionInfo(Uart * devicePtr);
+int enterDeepSleep(Uart * devicePtr, unsigned int time);
 int sendNLCR(Uart * devicePtr);
 
+/****************************** WiFi Control Functions ************************/
+int getWiFiMode(Uart * devicePtr);
+int setWiFiMode(Uart * devicePtr, unsigned int mode);
+int getCurrentAP(Uart * devicePtr);
+int setCurrentAP(Uart * devicePtr, char * ssid, char * pwd, char * bssid);
+int listAvailableAPs(Uart * devicePtr, char * ssid);
+int setDHCPmode(Uart * devicePtr, u8 operate, u8 mode);
+int getDHCPmode(Uart * devicePtr);
+int getSoftAPConfiguration(Uart * devicePtr);
+int setSoftAPConfiguration(Uart * devicePtr, char * ssid, char * pwd,
+    u8 channel, u8 encryption, u8 maxConn, u8 hidden);
+int listCurrentSoftAPConnections(Uart * devicePtr);
+
+/**************************** TCP/IP Control Utilities ************************/
+int getConnectionStatus(Uart * devicePtr);
+int establishTCPConnection(Uart * devicePtr, char * remoteIP,
+     int remotePort, int TCP_KeepAlive);
+int TCPsend(Uart * devicePtr, u8 * data, int length);
+
+
+
+/************************ AxiUartLite Control Functions ***********************/
 int setupInterrupt(Uart * devicePtr, u32 interruptDeviceID, u32 interruptID);
 void uartRecvHandler(void * CallBackRef, unsigned int EventData);
+void uartSendHandler(void * CallBackRef, unsigned int EventData);
 
 #endif  /* end of protection macro */
---
+++ simpleServer.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+from os import curdir, sep
+import cgi
+
+blink = True
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	def do_HEAD(s):
+		s.send_response(200)
+		s.send_header("Content-type", "text/html")
+		s.end_headers()
+	def do_GET(s):
+		# Respond to a GET request.
+		if s.path == "/":
+			s.path = "/index.html"
+
+		try:
+			sendReply = False
+			if s.path.endswith(".html"):
+				mimetype='text/html'
+				sendReply = True
+			if s.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendReply = True
+			if s.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendReply = True
+			if s.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendReply = True
+			if s.path.endswith(".css"):
+				mimetype='text/css'
+				sendReply = True
+			if s.path.endswith(".png"):
+				mimetype='image/png'
+				sendReply = True
+
+
+			if sendReply == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + s.path)
+				s.send_response(200)
+				s.send_header('Content-type',mimetype)
+				s.end_headers()
+				s.wfile.write(f.read())
+				f.close()
+			return
+
+		except IOError:
+			s.send_error(404,'File Not Found: %s' % s.path)
+
+	def do_POST(s):
+		global blink
+		if s.path == "/blink":
+			form = cgi.FieldStorage(
+				fp = s.rfile,
+				headers = s.headers,
+				environ = { 'REQUEST_METHOD': 'POST',
+		                 'CONTENT_TYPE': s.headers['Content-Type'],
+			})
+
+			if (form["cmd"].value == "stop blinking"):
+				print "Stop blinking"
+				blink = False
+			else:
+				blink = True
+				print "Start blinking"
+
+			#Redirect the browser on the main page
+			s.send_response(302)
+			s.send_header('Location','/')
+			s.end_headers()
+			return
+
+
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), MyHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.serve_forever()
